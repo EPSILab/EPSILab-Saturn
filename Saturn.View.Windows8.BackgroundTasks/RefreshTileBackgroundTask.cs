dabbc8d90bd3d1e6f21ca53570980f5e9d6fca0d
jbvigneron <jb.vigneron@live.fr>
2014-01-11 22:18:47 +0100

    Update NuGet packages and references

---
+++ Saturn.View.Windows8.BackgroundTasks/RefreshTileBackgroundTask.cs
@@ -0,0 +1,30 @@
+﻿using Saturn.View.Windows8.TileFactory;
+using Windows.ApplicationModel.Background;
+
+namespace SolarSystem.Saturn.Win8.BackgroundTasks
+{
+    /// <summary>
+    /// Background task which updates the application tile
+    /// </summary>
+    public sealed class RefreshTileBackgroundTask : IBackgroundTask
+    {
+        /// <summary>
+        /// Run the background task
+        /// </summary>
+        /// <param name="taskInstance">Returns task informations</param>
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+            try
+            {
+                CreateApplicationTileHelper.CreateAsync();
+            }
+            finally
+            {
+                // Informs the system task is finished
+                deferral.Complete();
+            }
+        }
+    }
+}
---
+++ Saturn.View.Windows8.TileFactory/Resources/ResourcesRsxAccessor.cs
@@ -0,0 +1,46 @@
+﻿using Windows.ApplicationModel.Resources;
+
+namespace Saturn.View.Windows8.TileFactory.Resources
+{
+    /// <summary>
+    /// A helper to access to the project ressources file
+    /// </summary>
+    static class ResourcesRsxAccessor
+    {
+        #region Static constructor
+
+        /// <summary>
+        /// Static constructor
+        /// </summary>
+        static ResourcesRsxAccessor()
+        {
+            const string filename = "Ressources";
+            ResourceLoader = new ResourceLoader(filename);
+        }
+
+        #endregion
+
+        #region Attributes
+
+        /// <summary>
+        /// Access to the ressource file
+        /// </summary>
+        private static readonly ResourceLoader ResourceLoader;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Get a string from the ressource file
+        /// </summary>
+        /// <param name="resource">Resource key</param>
+        /// <returns>Resource value</returns>
+        public static string GetString(string resource)
+        {
+            return ResourceLoader.GetString(resource);
+        }
+
+        #endregion
+    }
+}
